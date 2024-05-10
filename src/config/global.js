e47692cbe31473c6ac027ad3cdcd02b4082bc643
Andrea Botello <andrea.botellor@misena.edu.co>
2024-05-10 15:18:20 -0500

    Avance

--- src/config/global.js
+++ src/config/global.js
@@ -1,8 +1,9 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
+    componenteFormativo: 'Reconocimiento de la estructura corporal',
+    descripcionCurso:
+      'El programa Técnico en Patronaje Industrial de Prendas de Vestir, a través de diferentes componentes como “Reconocimiento de la estructura corporal”, da respuesta a necesidades de la región, del sector empresarial y sus trabajadores, frente a la capacitación de mano de obra calificada en el desarrollo y escalado de patrones de prendas de vestir de manera manual y sistematizada.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
@@ -31,13 +32,33 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Conceptos de Pre-Patronaje',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
+            titulo: 'Clasificación de las medidas',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Análisis de cuadros de tallas',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Concepto de Patronaje - Pasos del patronaje',
+            hash: 't_1_3',
+          },
+          {
+            numero: '1.4',
+            titulo: 'Patronaje',
+            hash: 't_1_4',
+          },
+          {
+            numero: '1.5',
+            titulo: 'Elaboración de básicos Infantil',
+            hash: 't_1_5',
           },
         ],
       },
@@ -45,14 +66,51 @@ export default {
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Alistamiento para elaboración de muestra física 1.',
         desarrolloContenidos: true,
-      },
-      {
-        nombreRuta: 'tema3',
-        numero: '3',
-        titulo: 'Titulo de primer nivel',
-        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Tipos de máquinas.',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Partes de la máquina',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Alimentación o transporte',
+            hash: 't_2_3',
+          },
+          {
+            numero: '2.4',
+            titulo: 'Generaciones de máquinas',
+            hash: 't_1_4',
+          },
+          {
+            numero: '2.5',
+            titulo: 'Clasificación, nomenclatura, Puntas, Calibres y usos.',
+            hash: 't_1_5',
+          },
+          {
+            numero: '2.6',
+            titulo:
+              'Relación de elementos de costura: Tela, Aguja, puntada, hilo, ajuste.',
+            hash: 't_2_6',
+          },
+          {
+            numero: '2.7',
+            titulo: 'Calibrar.',
+            hash: 't_2_7',
+          },
+          {
+            numero: '2.8',
+            titulo: 'Desarrollo de operaciones básico para ensamble de piezas.',
+            hash: 't_2_8',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -116,18 +174,37 @@ export default {
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Estándar',
+      significado:
+        'Que sirve de patrón, modelo o punto de referencia para medir o valorar cosas de la misma especie.',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: 'Talla',
+      significado:
+        'Expresión normalizada que permite identificar a las personas para el uso de prendas de vestir.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Equipo de Desarrollo Curricular, SENA Programa Técnico en Patronaje Industrial de Prendas de Vestir, Centro de Manufactura en Textil y Cuero, Distrito Capital, 2020.',
       link: '',
+    },
+    {
+      referencia:
+        'Equipo de Desarrollo Curricular, SENA Programa Tecnólogo en Desarrollo de Colecciones para la Industria de la Moda, 2020.',
+      link: '',
+    },
+    {
+      referencia:
+        'Gutiérrez, L. y Moncayo, A. (2011). Manual de patronaje básico e interpretación de diseños. Bogotá: SENA, Japan Inernational Cooperation Agency "jica", 2011.',
+      link: '',
+    },
+    {
+      referencia:
+        'Pulgarín, B. y Ulloa, J. (1998). La evolución de las máquinas y su aplicación en la industria de la confección Colombiana.',
+      link:
+        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000009660',
     },
   ],
   creditos: [
@@ -140,10 +217,9 @@ export default {
           centro: 'Dirección General',
         },
         {
-          nombre: 'Olga Constanza Bermúdez Jaimes',
+          nombre: 'Claudia Johanna Gómez Pérez',
           cargo: 'Responsable de Línea de Producción',
-          centro:
-            'Centro de Servicios de Salud - Regional Antioquia',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
     },
@@ -153,8 +229,7 @@ export default {
         {
           nombre: 'Nombre responsable',
           cargo: 'Nombre del rol',
-          centro:
-            'Centro de Servicios de Salud - Regional Antioquia',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
       ],
     },
@@ -164,26 +239,22 @@ export default {
         {
           nombre: 'Nombre responsable',
           cargo: 'Diseñador de Contenidos Digitales',
-          centro:
-            'Centro de Servicios de Salud - Regional Antioquia',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Andrea Paola Botello De la Rosa',
           cargo: 'Desarrollador Fullstack',
-          centro:
-            'Centro de Servicios de Salud - Regional Antioquia',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
         {
           nombre: 'Nombre',
           cargo: 'Animador y Producción audiovisual',
-          centro:
-            'Centro de Servicios de Salud - Regional Antioquia',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
         {
           nombre: 'Nombre',
           cargo: 'Actividad Didáctica',
-          centro:
-            'Centro de Servicios de Salud - Regional Antioquia',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
       ],
     },
@@ -193,27 +264,23 @@ export default {
         {
           nombre: 'Luis Gabriel Urueta Alvarez',
           cargo: 'Validador de Recursos Educativos Digitales',
-          centro:
-            'Centro de Servicios de Salud - Regional Antioquia',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
         {
           nombre: 'Jaime Hernan Tejada',
           cargo: 'Validador de Recursos Educativos Digitales',
-          centro:
-            'Centro de Servicios de Salud - Regional Antioquia',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
         {
           nombre: 'Daniel Ricardo Mutis Gómez',
           cargo: 'Evaluador para contenidos inclusivos y accesibles',
-          centro:
-            'Centro de Servicios de Salud - Regional Antioquia',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
         {
           nombre: 'Margarita Marcela Medrano',
           cargo: 'Evaluador para contenidos inclusivos y accesibles',
-          centro:
-            'Centro de Servicios de Salud - Regional Antioquia',
-        }
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+        },
       ],
     },
   ],
