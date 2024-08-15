860c643164f01f9effa9229b56fad6005711aae6
PABG1990 <110750475+PABG1990@users.noreply.github.com>
2024-08-15 12:39:42 -0500

    Act

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,15 +7,15 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/Falso_Verdadero.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/completar.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
           h2 Conceptos de estructura corporal y tipos de máquinas.
-        p.mb-4 #[b Identificar la apropiación de los contenidos en la temática sobre reconocimiento de la estructura corporal.] 
+        p.mb-4 Identificar la apropiación de los contenidos en la temática sobre reconocimiento de la estructura corporal.
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Tipo de actividad
+              p.fw-bold.mb-0 Completar Espacios
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
                 span Realizar
